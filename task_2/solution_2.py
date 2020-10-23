60ccbd21a34c2cc768884060bdb62c93e52cab5a
EAShepel <71960636+EAShepel@users.noreply.github.com>
2020-10-23 22:30:12 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,12 @@
-# your code here
 user_input = input()
-# your code here
+most_frequent_character = user_input[0]
+c = 0
+for i in user_input:
+    count = 0
+    for j in user_input:
+        if i == j:
+            count = count + 1
+    if count > c:
+        c = count
+        most_frequent_character = i
 print(most_frequent_character)
