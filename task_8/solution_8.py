6ddcc32ad6055579748c2cad59d0530001e12f3b
EAShepel <71960636+EAShepel@users.noreply.github.com>
2020-10-28 23:59:39 +0300

    Update solution_8.py

--- task_8/solution_8.py
+++ task_8/solution_8.py
@@ -1,1 +1,13 @@
-# your code here
+n = input()
+num = int(n)
+count = 0
+summ = 0
+for i in range(num):
+    count = count + 1
+    line = input()
+    if line == '0':
+        break
+    count = count-1
+    summ = summ + count
+
+print(summ/(count))
