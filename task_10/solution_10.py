a5d34d0fb3ab551c93be82027518fdbd1f6bc7ff
EAShepel <71960636+EAShepel@users.noreply.github.com>
2020-10-29 00:00:53 +0300

    Update solution_10.py

--- task_10/solution_10.py
+++ task_10/solution_10.py
@@ -1,1 +1,17 @@
-# your code here
+line = input()
+count = 0
+a = str()
+b = str()
+for i in line:
+    if i == ' ':
+        line[count] == line[count+1]
+    if line[count] == line [-count-1]:
+        a = True
+    else:
+        b = False
+    count += 1
+c = (a,b)
+if False not in c:
+    print('полином')
+else:
+    print('неполином')
