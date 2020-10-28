ab2aebec3d12348d88f9a0f56e6b2a34beaa3fcd
EAShepel <71960636+EAShepel@users.noreply.github.com>
2020-10-28 23:09:05 +0300

    Update solution_9.py

--- task_9/solution_9.py
+++ task_9/solution_9.py
@@ -1,1 +1,8 @@
-# your code here
+for n in range(101):
+    sum = 0
+    for i in range(1, 2*n, 2):
+        sum = sum + i
+    if sum == n*n:
+        print(n, '-', True)
+    else:
+        print(n, '-', False)
